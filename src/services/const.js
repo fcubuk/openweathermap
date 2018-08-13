0437dbb9357c07a40eb1d3eab293824c099b3bfb
fcubuk <fcubuk@gmail.com>
2018-08-13 16:41:49 +0100

    Update const.js

--- src/services/const.js
+++ src/services/const.js
@@ -1,2 +1,2 @@
 export const CITIES_DATA_URL = '../data/cities.js';
-export const WEATHER_DATA_URL = 'http://api.openweathermap.org/data/2.5/forecast?&mode=json&appid=bd5e378503939ddaee76f12ad7a97608';  // other apikey
+export const WEATHER_DATA_URL = 'http://api.openweathermap.org/data/2.5/forecast?&mode=json&appid=${APIKEY}';  // other apikey
